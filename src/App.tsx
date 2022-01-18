66fb9cb022abac339d901ef9866afe86cd82787a
LeonShus <leonshustov1995@gmail.com>
2022-01-18 19:33:39 +0300

    Add common components: SuperInput, SuperButton, SuperCheckbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import "./App.css";
+import SuperInputText from "./components/common/c1-SuperInputText/SuperInputText";
+import SuperButton from "./components/common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "./components/common/c3-SuperCheckbox/SuperCheckbox";
 
 export const App = () => {
     return (
         <div>
-            HW14
+            <SuperInputText/>
+            <SuperButton>
+                Click
+            </SuperButton>
+            <SuperCheckbox/>
         </div>
     );
 }
