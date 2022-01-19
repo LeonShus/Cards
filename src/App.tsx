b75c66adb6ab7d0146efd0146358b46548f9fb6a
LeonShus <leonshustov1995@gmail.com>
2022-01-19 18:41:06 +0300

    add links and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
 import React from "react";
 import "./App.css";
-import SuperInputText from "./components/common/c1-SuperInputText/SuperInputText";
-import SuperButton from "./components/common/c2-SuperButton/SuperButton";
-import SuperCheckbox from "./components/common/c3-SuperCheckbox/SuperCheckbox";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {LoginPage} from "./components/pages/p1- loginization/l1-login/login-page";
+import {RegistrationPage} from "./components/pages/p1- loginization/l2-registration/registration-page";
+import {ProfilePage} from "./components/pages/p2-profile/profile-page";
+import {CreateNewPassPage} from "./components/pages/p1- loginization/l4-create-new-pass/create-new-pass-page";
+import {PassRecoveryPage} from "./components/pages/p1- loginization/l3-pass-recovery/pass-recovery-page";
+import {Error404Page} from "./components/pages/p3-error/error404-page";
+import {TestPage} from "./components/pages/p4-test/test-page";
 
 export const App = () => {
     return (
-        <div>
-            <SuperInputText/>
-            <SuperButton>
-                Click
-            </SuperButton>
-            <SuperCheckbox/>
-        </div>
+        <BrowserRouter>
+            <nav>
+                <Link to={"login"}>Login</Link>
+                <Link to={"registration"}>Registration</Link>
+                <Link to={"recovery"}>Recovery pass</Link>
+                <Link to={"pass"}>New Pass</Link>
+                <Link to={"error"}>404</Link>
+                <Link to={"test"}>Test</Link>
+            </nav>
+            <Routes>
+                <Route path={"login"} element={<LoginPage/>}/>
+                <Route path={"registration"} element={<RegistrationPage/>}/>
+                <Route path={"profile"} element={<ProfilePage/>}/>
+                <Route path={"recovery"} element={<PassRecoveryPage/>}/>
+                <Route path={"pass"} element={<CreateNewPassPage/>}/>
+                <Route path={"error"} element={<Error404Page/>}/>
+                <Route path={"test"} element={<TestPage/>}/>
+            </Routes>
+        </BrowserRouter>
     );
 }
--- src/components/pages/p4-test/test-page.tsx
+++ src/components/pages/p4-test/test-page.tsx
@@ -1,9 +1,16 @@
 import React from "react"
+import SuperInputText from "../../common/c1-SuperInputText/SuperInputText";
+import SuperButton from "../../common/c2-SuperButton/SuperButton";
+import SuperCheckbox from "../../common/c3-SuperCheckbox/SuperCheckbox";
 
 export const TestPage = () => {
     return (
         <div>
-            TestPage
+            <SuperInputText/>
+            <SuperButton>
+                Click
+            </SuperButton>
+            <SuperCheckbox/>
         </div>
     )
 }
