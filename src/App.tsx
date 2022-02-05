6ec7e50710e48f290ca5f820efe2ebfd0726d97d
LeonShus <leonshustov1995@gmail.com>
2022-02-05 15:21:42 +0300

    Add formik,yup,sass. refactor app and index css

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import styles from "./App.module.scss"
 import {HashRouter, Link, Route, Routes} from "react-router-dom";
 import {LoginPage} from "./components/pages/p1- loginization/l1-login/login-page";
 import {RegistrationPage} from "./components/pages/p1- loginization/l2-registration/registration-page";
@@ -11,24 +11,26 @@ import {TestPage} from "./components/pages/p4-test/test-page";
 
 export const App = () => {
     return (
-        <HashRouter>
-            <nav>
-                <Link to={"login"}>Login</Link>
-                <Link to={"registration"}>Registration</Link>
-                <Link to={"recovery"}>Recovery pass</Link>
-                <Link to={"pass"}>New Pass</Link>
-                <Link to={"error"}>404</Link>
-                <Link to={"test"}>Test</Link>
-            </nav>
-            <Routes>
-                <Route path={"login"} element={<LoginPage/>}/>
-                <Route path={"registration"} element={<RegistrationPage/>}/>
-                <Route path={"profile"} element={<ProfilePage/>}/>
-                <Route path={"recovery"} element={<PassRecoveryPage/>}/>
-                <Route path={"pass"} element={<CreateNewPassPage/>}/>
-                <Route path={"error"} element={<Error404Page/>}/>
-                <Route path={"test"} element={<TestPage/>}/>
-            </Routes>
-        </HashRouter>
+        <div>
+            <HashRouter>
+                <nav>
+                    <Link to={"login"}>Login</Link>
+                    <Link to={"registration"}>Registration</Link>
+                    <Link to={"recovery"}>Recovery pass</Link>
+                    <Link to={"pass"}>New Pass</Link>
+                    <Link to={"error"}>404</Link>
+                    <Link to={"test"}>Test</Link>
+                </nav>
+                <Routes>
+                    <Route path={"login"} element={<LoginPage/>}/>
+                    <Route path={"registration"} element={<RegistrationPage/>}/>
+                    <Route path={"profile"} element={<ProfilePage/>}/>
+                    <Route path={"recovery"} element={<PassRecoveryPage/>}/>
+                    <Route path={"pass"} element={<CreateNewPassPage/>}/>
+                    <Route path={"error"} element={<Error404Page/>}/>
+                    <Route path={"test"} element={<TestPage/>}/>
+                </Routes>
+            </HashRouter>
+        </div>
     );
 }
