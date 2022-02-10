83de50c1b07d2bdc40ea4c5fe0523eda89dc7ae3
LeonShus <leonshustov1995@gmail.com>
2022-02-10 21:15:48 +0300

    Add userName to profile page

--- src/components/pages/p2-profile/profile-page.tsx
+++ src/components/pages/p2-profile/profile-page.tsx
@@ -1,9 +1,17 @@
 import React from "react"
+import {useSelector} from "react-redux";
+import {AppStateType} from "../../bll/b2-store/store";
 
 export const ProfilePage = () => {
+
+    const userName = useSelector<AppStateType, string>(state => state.login.userData.name)
+
     return(
         <div>
             ProfilePage
+            <div>
+                {userName}
+            </div>
         </div>
     )
 }
