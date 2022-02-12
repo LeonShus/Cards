0dc582bebcf1af4bd2b41f5b3ef2d33c7ebde0ee
LeonShus <leonshustov1995@gmail.com>
2022-02-12 20:44:51 +0300

    Add some default styles restyling components + create mixins

--- src/common/c2-components/c6-Header/Header.tsx
+++ src/common/c2-components/c6-Header/Header.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import {Navigation} from "../c7-Navigation/Navigation";
+import styles from "./Header.module.scss"
 
 export const Header = () => {
-    return(
-        <div>
+    return (
+        <div className={styles.container}>
             <Navigation/>
         </div>
     )
