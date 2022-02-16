80f2b8638d9037e46ea3132251bdac67e514ba33
LeonShus <leonshustov1995@gmail.com>
2022-02-16 19:08:17 +0300

    Create groupButton component with MUI

--- src/common/c2-components/c9-slider/s1-range-slider.tsx
+++ src/common/c2-components/c9-slider/s1-range-slider.tsx
@@ -2,10 +2,26 @@ import React from "react"
 import styles from "./s1-range-slider.module.scss"
 import Slider from "@mui/material/Slider";
 
-export const RangeSlider = () => {
+type RangeSliderPropsType = {
+    minMax: number[]
+}
+
+export const RangeSlider = ({minMax}: RangeSliderPropsType) => {
+
+    const [value, setValue] = React.useState<number[]>(minMax);
+
+    const handleChange = (event: Event, newValue: number | number[]) => {
+        setValue(newValue as number[]);
+    };
+
     return(
         <div className={styles.container}>
-            <Slider />
+            <Slider
+                value={value}
+                onChange={handleChange}
+                valueLabelDisplay="on"
+                sx={{color: "#45007A"}}
+            />
         </div>
     )
 }
--- src/components/pages/p5-packs-list/packs-list.tsx
+++ src/components/pages/p5-packs-list/packs-list.tsx
@@ -1,12 +1,27 @@
 import React from "react"
 import styles from "./packs-list.module.scss"
 import {RangeSlider} from "../../../common/c2-components/c9-slider/s1-range-slider";
+import {CustomButtonGroup} from "../../../common/c2-components/c2-CustomButton/c1-custom-button-group";
 
 export const PacksList = () => {
+
+    const minRange = 10
+    const maxRange = 100
+
     return(
         <div className={styles.container}>
             <div className={styles.filterContainer}>
-                <RangeSlider/>
+
+                <div className={styles.showContainer}>
+                    <h5>Number of cards</h5>
+                    <CustomButtonGroup buttonsName={["My", "All"]}/>
+                </div>
+
+                <div className={styles.sliderContainer}>
+                    <h5>Number of cards</h5>
+                    <RangeSlider minMax={[minRange, maxRange]}/>
+                </div>
+
             </div>
             <div className={styles.listContainer}>
                 packList
